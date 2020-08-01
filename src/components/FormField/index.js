46d79bc5bb8f495c9218461221a51404bf53e364
André Nechio <kgsnechio@gmail.com>
2020-08-01 18:49:46 -0300

    commit

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -2,10 +2,15 @@ import React from 'react'
 
 function FormField({ label, type, name, value, onChange }) {
 
+    const fieldId = `id_${name}`
+    
     return(
-        <label>
+        <label
+            htmlFor={fieldId}
+        >
             {label} :
             <input
+                id={fieldId}
                 type={type}
                 name={name}
                 value={value}
